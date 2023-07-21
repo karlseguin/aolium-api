4772304426eefaf4d2022b8cd28a9bfaf5c5871f
Karl Seguin <k@openmymind.io>
2023-07-21 16:28:58 +0800

    list post paging

--- src/web/posts/index.zig
+++ src/web/posts/index.zig
@@ -8,6 +8,7 @@ const posts = @import("_posts.zig");
 
 const web = posts.web;
 const pondz = web.pondz;
+const Allocator = std.mem.Allocator;
 
 var index_validator: *validate.Object(void) = undefined;
 
@@ -15,6 +16,7 @@ pub fn init(builder: *validate.Builder(void)) void {
 	index_validator = builder.object(&.{
 		builder.field("username", builder.string(.{.required = true, .max = pondz.MAX_USERNAME_LEN, .trim = true})),
 		builder.field("html", builder.boolean(.{.parse = true})),
+		builder.field("page", builder.int(u16, .{.parse = true, .min = 1})),
 	}, .{});
 }
 
@@ -27,22 +29,11 @@ pub fn handler(env: *pondz.Env, req: *httpz.Request, res: *httpz.Response) !void
 		return web.notFound(res, "username doesn't exist");
 	};
 
+	const page = input.get(u16, "page") orelse 1;
 	const html = input.get(bool, "html") orelse false;
+	const fetcher = PostFetcher.init(req.arena, env, user, page, html);
 
-	var fetcher = PostFetcher{
-		.env = env,
-		.user = user,
-		.arena = res.arena,
-		.html = input.get(bool, "html") orelse false,
-	};
-
-	// 8 byte id + html bool
-	var cache_key_buf: [9]u8 = undefined;
-	@memcpy(cache_key_buf[0..8], std.mem.asBytes(&user.id));
-	cache_key_buf[8] = if (html) 1 else 0;
-	const cache_key = cache_key_buf[0..];
-
-	const cached_response = (try app.http_cache.fetch(*PostFetcher, cache_key, getPosts, &fetcher, .{.ttl = 300})).?;
+	const cached_response = (try app.http_cache.fetch(*const PostFetcher, &fetcher.cache_key, getPosts, &fetcher, .{.ttl = 300})).?;
 	defer cached_response.release();
 	res.header("Cache-Control", "private,max-age=30");
 	try cached_response.value.write(res);
@@ -57,9 +48,29 @@ fn getUser(app: *pondz.App, username: []const u8) !?pondz.User {
 
 const PostFetcher = struct {
 	html: bool,
+	page: u16,
 	env: *pondz.Env,
 	user: pondz.User,
+	cache_key: [11]u8,
 	arena: std.mem.Allocator,
+
+	fn init(arena: Allocator, env: *pondz.Env, user: pondz.User, page: u16, html: bool) PostFetcher {
+		// id + page + html
+		// 8  + 2    + 1
+		var cache_key: [11]u8 = undefined;
+		@memcpy(cache_key[0..8], std.mem.asBytes(&user.id));
+		@memcpy(cache_key[8..10], std.mem.asBytes(&page));
+		cache_key[10] = if (html) 1 else 0;
+
+		return .{
+			.env = env,
+			.user = user,
+			.page = page,
+			.html = html,
+			.arena = arena,
+			.cache_key = cache_key,
+		};
+	}
 };
 
 fn getPosts(fetcher: *const PostFetcher, _: []const u8) !?web.CachedResponse {
@@ -67,14 +78,16 @@ fn getPosts(fetcher: *const PostFetcher, _: []const u8) !?web.CachedResponse {
 	const html = fetcher.html;
 	const user = fetcher.user;
 
+	const offset = (fetcher.page - 1) * 20;
+
 	const sql =
 		\\ select id, type, title, text, created, updated
 		\\ from posts
 		\\ where user_id = ?1
 		\\ order by created desc
-		\\ limit 20
+		\\ limit 20 offset ?2
 	;
-	const args = .{user.id};
+	const args = .{user.id, offset};
 
 	const app = env.app;
 	var sb = try app.buffers.acquireWithAllocator(fetcher.arena);
@@ -100,7 +113,6 @@ fn getPosts(fetcher: *const PostFetcher, _: []const u8) !?web.CachedResponse {
 		// and then using json.stringify(.{.posts = posts}). But if we did that, we'd
 		// need to dupe the text values so that they outlive a single iteration of the
 		// loop. Instead, we serialize each post immediately and glue everything together.
-
 
 		// TODO: can/should heavily optimzie this, namely by storing pre-generated
 		// json and html blobs that we just glue together.
